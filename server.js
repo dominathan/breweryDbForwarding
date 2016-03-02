b0225da8c931cbfa133825b80e10d7e495084754
Nathan Hall <nathan.mh@gmail.com>
2016-03-02 10:52:12 -0500

    add cors to server

--- server.js
+++ server.js
@@ -11,13 +11,22 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // app.get('/', function(req,response) {
 //   response.sendFile('public');
 // });
 
 app.post('/any-request/', function(req,res,next) {
   console.log('test', req.body);
-  var url = req.body.url
+  var url = req.body
+  // var options = {
+  //   url: req.body.url
+  // }
   request.get({url: url}, function(err, response) {
     if(err) {
       console.log("OH FUCK", err);
